79db5517b7b80b34c8fd10de22b5ccea84c1d07a
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-02-02 09:50:20 +0200

    Initial commit

---
+++ app/src/main/java/ws/tilda/anastasia/bitsandpizzas/MainActivity.java
@@ -0,0 +1,49 @@
+package ws.tilda.anastasia.bitsandpizzas;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreatePanelMenu(int featureId, Menu menu) {
+        return super.onCreatePanelMenu(featureId, menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_create_order:
+
+                return true;
+            case R.id.action_settings:
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+
+
+
+
+
+}
