0947a4030d728089a4029892c159a7220e784d88
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-02-02 15:43:30 +0200

    created onClickListener for drawer

--- app/src/main/java/ws/tilda/anastasia/bitsandpizzas/MainActivity.java
+++ app/src/main/java/ws/tilda/anastasia/bitsandpizzas/MainActivity.java
@@ -1,10 +1,15 @@
 package ws.tilda.anastasia.bitsandpizzas;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ShareActionProvider;
 
@@ -13,12 +18,55 @@ public class MainActivity extends Activity {
     private String[] titles;
     private ListView drawerList;
 
+    private class DrawerItemClickListener implements ListView.OnClickListener {
+
+        public void onItemClick(AdapterView<?>parent, View view, int position, long id) {
+            selectItem(position);
+
+        }
+
+        private void selectItem(int position) {
+            Fragment fragment;
+
+            switch(position) {
+                case 1:
+                    fragment = new PizzaFragment();
+                    break;
+                case 2:
+                    fragment = new PastaFragment();
+                    break;
+                case 3:
+                    fragment = new StoresFragment();
+                    break;
+                default:
+                    fragment = new TopFragment();
+                    break;
+            }
+
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        }
+
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
+        titles = getResources().getStringArray(R.array.titles);
+        drawerList = (ListView) findViewById(R.id.drawer);
+        drawerList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, titles));
+        drawerList.setOnClickListener(new DrawerItemClickListener());
+     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
