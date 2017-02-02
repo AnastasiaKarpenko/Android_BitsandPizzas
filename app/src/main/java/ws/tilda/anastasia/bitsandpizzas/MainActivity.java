0581eef3a7869c5384fc3eaab08e28af221a26ee
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-02-02 11:08:16 +0200

    Set the action bar and create order activity, share acticity

--- app/src/main/java/ws/tilda/anastasia/bitsandpizzas/MainActivity.java
+++ app/src/main/java/ws/tilda/anastasia/bitsandpizzas/MainActivity.java
@@ -1,12 +1,14 @@
 package ws.tilda.anastasia.bitsandpizzas;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ShareActionProvider;
 
 public class MainActivity extends Activity {
+    private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,22 +16,29 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreatePanelMenu(int featureId, Menu menu) {
-        return super.onCreatePanelMenu(featureId, menu);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) menuItem.getActionProvider();
+        setIntent("This is example text");
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
             case R.id.action_create_order:
-
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
                 return true;
             case R.id.action_settings:
 
