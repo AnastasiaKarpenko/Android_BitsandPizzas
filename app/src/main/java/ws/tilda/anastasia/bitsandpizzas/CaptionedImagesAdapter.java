ae5a0df07db7e7febc88472522c9c43522f28ff8
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-02-06 15:28:42 +0200

    added interface Listener to Adapter (for Recycler view to react on clicks)

--- app/src/main/java/ws/tilda/anastasia/bitsandpizzas/CaptionedImagesAdapter.java
+++ app/src/main/java/ws/tilda/anastasia/bitsandpizzas/CaptionedImagesAdapter.java
@@ -4,14 +4,21 @@ import android.graphics.drawable.Drawable;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 
 public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
 
     private String[] captions;
     private int[] imageIds;
+    private Listener listener;
+
+    public static interface Listener {
+        public void onClick(int position);
+    }
 
     public CaptionedImagesAdapter(String[] captions, int[] imageIds) {
         this.captions = captions;
@@ -30,6 +37,11 @@ public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImages
 
     }
 
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+
+
     @Override
     public CaptionedImagesAdapter.ViewHolder onCreateViewHolder(
             ViewGroup parent, int viewType) {
@@ -41,7 +53,7 @@ public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImages
 
 
     @Override
-    public void onBindViewHolder(CaptionedImagesAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(CaptionedImagesAdapter.ViewHolder holder, final int position) {
         CardView cardView = holder.cardView;
         ImageView imageView = (ImageView) cardView.findViewById(R.id.info_image);
         Drawable drawable = cardView.getResources().getDrawable(imageIds[position]);
@@ -49,6 +61,14 @@ public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImages
         imageView.setContentDescription(captions[position]);
         TextView textView = (TextView) cardView.findViewById(R.id.info_text);
         textView.setText(captions[position]);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(listener != null) {
+                    listener.onClick(position);
+                }
+            }
+        });
     }
 
     @Override
--- app/src/main/java/ws/tilda/anastasia/bitsandpizzas/PizzaDetailActivity.java
+++ app/src/main/java/ws/tilda/anastasia/bitsandpizzas/PizzaDetailActivity.java
@@ -1,6 +1,6 @@
 package ws.tilda.anastasia.bitsandpizzas;
 
-import android.content.Context;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
