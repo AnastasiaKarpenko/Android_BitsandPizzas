5a4f7523b26d282f9989d6734ba7f844fbea5b66
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-02-06 15:46:57 +0200

    set the listener in PizzaMaterialFragment, the listener will implement onClickMethod to load PizzaDetailActivity

--- app/src/main/java/ws/tilda/anastasia/bitsandpizzas/PizzaMaterialFragment.java
+++ app/src/main/java/ws/tilda/anastasia/bitsandpizzas/PizzaMaterialFragment.java
@@ -2,6 +2,7 @@ package ws.tilda.anastasia.bitsandpizzas;
 
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -42,6 +43,15 @@ public class PizzaMaterialFragment extends Fragment {
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         pizzaRecycler.setLayoutManager(layoutManager);
+
+
+        //Method onClick implementation of Listener interface which launches Pizza Detail Activity
+        adapter.setListener(new CaptionedImagesAdapter.Listener() { public void onClick(int position) {
+            Intent intent = new Intent(getActivity(), PizzaDetailActivity.class);
+            intent.putExtra(PizzaDetailActivity.EXTRA_PIZZANO, position);
+            getActivity().startActivity(intent);
+          }
+        });
 
         return pizzaRecycler;
 
